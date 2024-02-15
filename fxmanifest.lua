2d839c4f407f8a64047da2d4b65ac9b5a4542437
mP <109157032+ikparu0910@users.noreply.github.com>
2024-02-14 23:01:53 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,5 @@
-shared_script '@mp_var/ai_module_fg-obfuscated.lua'
 shared_script '@mp_var/shared_fg-obfuscated.lua'
-
-
-
-
+shared_script '@mp_var/ai_module_fg-obfuscated.lua'
 --[[ FX Information ]]
                        --
 fx_version 'cerulean'
@@ -11,7 +7,6 @@ use_experimental_fxv2_oal 'yes'
 lua54 'yes'
 games { 'rdr3', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
 --[[ Resource Information ]]
                              --
 name 'ox_lib'
@@ -20,16 +15,13 @@ version '3.16.2'
 license 'LGPL-3.0-or-later'
 repository 'https://github.com/overextended/ox_lib'
 description 'A library of shared functions to utilise in other resources.'
-
 --[[ Manifest ]]
                  --
 dependencies {
     '/server:7290',
     '/onesync',
 }
-
 ui_page 'web/build/index.html'
-
 files {
     'init.lua',
     'imports/**/client.lua',
@@ -38,19 +30,15 @@ files {
     'web/build/**/*',
     'locales/*.json',
 }
-
 shared_script 'resource/init.lua'
-
 shared_scripts {
     'resource/**/shared.lua',
     -- 'resource/**/shared/*.lua'
 }
-
 client_scripts {
     'resource/**/client.lua',
     'resource/**/client/*.lua'
 }
-
 server_scripts {
     'imports/callback/server.lua',
     'resource/**/server.lua',
